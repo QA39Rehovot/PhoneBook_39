9a31e960aad12de63079ee6a61b38a8839d468a4
webinartelran <webinartelran@gmail.com>
2023-09-03 19:32:53 +0300

    CW11_TestBase

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -1,24 +1,9 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class RegistrationTests {
-
-    WebDriver wd;
-
-    @BeforeMethod
-    public void init(){
-        wd = new ChromeDriver();
-        wd.navigate().to("https://telranedu.web.app/home");
-        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-    }
+public class RegistrationTests extends TestBase{
 
     @Test
     public void registrationPositiveTest(){
@@ -40,22 +25,31 @@ public class RegistrationTests {
         // click on button registration
         wd.findElement(By.xpath("//button[2]")).click();
         // assert
-//        pause(3000);
+        pause(3000);
         Assert.assertTrue(wd.findElements(By.tagName("button")).size() > 0);
-
     }
+    @Test
+    public void registrationNegativeTestWrongEmail(){
+        // open login form
+        wd.findElement(By.xpath("//*[.='LOGIN']")).click();
+        // fill login form
+        int i = (int)(System.currentTimeMillis()/1000)%3600;
 
-    @AfterMethod
-    public void tearDown(){
-        pause(5000);
-        wd.quit();
-    }
+        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("abc_" + i + "def.com");
 
-    public void pause(int millis){
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
+        passInput.click();
+        passInput.clear();
+        passInput.sendKeys("$Abcdef12345");
+
+        // click on button registration
+        wd.findElement(By.xpath("//button[2]")).click();
+        // assert
+        pause(3000);
+        Assert.assertTrue(isAlertPresent());
+
     }
 }
